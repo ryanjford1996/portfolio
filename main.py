102771a797d1ba05382cc168f8e6003b2758787e
ryanjford1996 <ryanjford1996@gmail.com>
2022-06-22 21:19:36 -0500

    functioning navbar, routes set

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-Bootstrap(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/projects')
+def showProjects():
+    return render_template('projects.html')
+
+@app.route('/resume')
+def showResume():
+    return render_template('my-resume.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
