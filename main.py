ba66dcbf20ba5593f83276b10d8e12e840fc39ca
ryanjford1996 <ryanjford1996@gmail.com>
2022-06-25 20:47:54 -0500

    finished card skeleton for future projects to add, resume page started

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 from flask import Flask, render_template, request
+import datetime
+import smtplib
+
+EMAIL = 'datigerace@yahoo.com'
+PASSWORD = 'irclovgwmytvoazb'
+todayYear = datetime.datetime.now().year
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', todayYear=todayYear)
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'GET':
+        return render_template('contact.html', todayYear=todayYear, msg_sent=False)
+    elif request.method == 'POST':
+        data = request.form
+        send_mail(name=data['name'], email=data['email'], phone=data['phone'], message=data['message'])
+        return render_template('contact.html', todayYear=todayYear, msg_sent=True)
 
 @app.route('/projects')
 def showProjects():
-    return render_template('projects.html')
+    return render_template('projects.html', todayYear=todayYear)
 
 @app.route('/resume')
 def showResume():
-    return render_template('my-resume.html')
+    return render_template('my-resume.html', todayYear=todayYear)
+
+def send_mail(name, email, phone, message):
+    email_message = f'Subject:New Message on your Website\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}'
+    with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL,
+                         password=PASSWORD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs=EMAIL,
+                            msg=email_message,
+                            )
+        connection.quit()
 
 if __name__ == "__main__":
     app.run(debug=True)
